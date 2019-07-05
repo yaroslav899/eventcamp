0d4cdae83ddd0cac0597e0e4bdfcc346fc378b36
ASTOUND\y.volokh <y.volokh@astoundcommerce.com>
2019-07-05 17:26:20 +0300

    Fixed issue with city for title

--- src/components/event-detail/index.js
+++ src/components/event-detail/index.js
@@ -4,6 +4,8 @@ import { Helmet } from 'react-helmet';
 import moment from 'moment';
 import { request } from '../../api';
 import store from '../../store';
+import { getValueFromParams } from '../../helper';
+import { cities } from '../../fixtures';
 import DetailPageView from './views/DetailPageView';
 import Loader from '../global/Loader';
 
@@ -36,11 +38,12 @@ class DetailPage extends Component {
 
     const date = moment(post.acf.dateOf, 'YYYY-MM-DD').format('Do MMM').split(' ');
     const getDateDay = moment(post.acf.dateOf, 'YYYY-MM-DD').format('dddd');
+    const city = getValueFromParams(cities, post.acf.cities, 'url', 'name');
 
     return (
       <Fragment>
         <Helmet>
-          <title itemProp="name" lang="uk">{`${post.title.rendered} - ${post.acf.cities}`}</title>
+          <title itemProp="name" lang="uk">{`${post.title.rendered} - ${date} | ${city}`}</title>
           <meta name="description" content={post.content.rendered.substr(0, 180)} />
           <meta name="keywords" content={post.acf.tags} />
           <meta property="og:title" content={post.title.rendered} />
