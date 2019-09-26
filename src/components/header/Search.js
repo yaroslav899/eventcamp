f9bf4fea879ac184bd7909c397d22a226b38f34a
ASTOUND\y.volokh <y.volokh@astoundcommerce.com>
2019-09-26 17:17:41 +0300

    Added functionality to search suggestion

---
+++ src/components/header/Search.js
@@ -0,0 +1,75 @@
+﻿import React, { PureComponent } from 'react';
+import SearchSuggestion from './SearchSuggestion';
+
+class Search extends PureComponent {
+  state = {
+    searchSuggestion: '',
+    eventList: null,
+    isShowSuggestion: false,
+  };
+
+  handleChange = (event) => {
+    const { urlSearch, minChars } = this.props;
+    const { searchSuggestion } = this.state;
+
+    if (searchSuggestion.length < minChars) {
+        this.setState({
+          searchSuggestion: event.target.value,
+          eventList: null,
+          isShowSuggestion: false,
+        });
+
+        return false;
+    }
+
+    this.setState({ searchSuggestion: event.target.value }, ()=>{
+        return fetch(urlSearch + this.state.searchSuggestion)
+            .then(response => response.json())
+            .then(response => {
+              if (response && response.length) {
+                this.setState({ eventList: response, isShowSuggestion: true });
+
+                return null;
+              }
+            }).catch(error => {
+              console.log('error');
+
+              return null;
+            });
+    });
+
+    return true;
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('asdas');
+  }
+
+  render() {
+    const { searchSuggestion, eventList, isShowSuggestion } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          id="search"
+          placeholder="Пошук"
+          className="search-suggest"
+          name="search"
+          onChange={this.handleChange}
+        />
+        {isShowSuggestion &&
+          <SearchSuggestion eventList={eventList}/>
+        }
+      </form>
+    );
+  }
+}
+
+Search.defaultProps = {
+  urlSearch: 'https://board.event-camp.org/wp-json/wp/v2/posts?&search=',
+  minChars: 3,
+};
+
+export default Search;
---
+++ src/components/header/SearchSuggestion.js
@@ -0,0 +1,28 @@
+﻿import React, { PureComponent } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getValueFromParams, createMarkupText } from '../../helper';
+import { categories } from '../../fixtures';
+
+class SearchSuggestion extends PureComponent {
+  render() {
+    const { eventList } = this.props;
+
+    let eventElement = eventList.map((event) => {
+        const category = getValueFromParams(categories, event.categories[0], 'id', 'url');
+        const url = `/events/${event.acf.cities}/${category}/${event.id}`;
+
+        return <li key={event.id} className="">
+          <NavLink to={url} className="events-item__title">
+            {event.title.rendered}
+          </NavLink>
+        </li>;
+      });
+    return (
+      <ul className="header__search-suggestion">
+        {eventElement}
+      </ul>
+    )
+  }
+}
+
+export default SearchSuggestion;
