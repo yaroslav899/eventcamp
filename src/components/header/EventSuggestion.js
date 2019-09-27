8903259e07f875ca74a92ceb61f6562022cc9ef1
ASTOUND\y.volokh <y.volokh@astoundcommerce.com>
2019-09-27 18:32:39 +0300

    Split of 2 components for suggest. Optimize mousedown click

---
+++ src/components/header/EventSuggestion.js
@@ -0,0 +1,25 @@
+﻿import React, { PureComponent } from 'react';
+import EventLocation from '../event-global/EventLocation';
+import EventDate from '../event-global/EventDate';
+
+class EventSuggestion extends PureComponent {
+  handleMouseDown = () => {
+    //ToDo The Issue should be solved by React tools
+    const { url } = this.props;
+    window.location = url;
+  }
+
+  render() {
+    const { eventID, title, city, address, date, category } = this.props;
+
+    return <li key={eventID} onMouseDown={this.handleMouseDown}>
+      <span dangerouslySetInnerHTML={title} />
+      <div>
+        <EventLocation className='header__search-suggestion-location' city={city} address={address} />
+        <EventDate className='header__search-suggestion-date' date={date} />
+      </div>
+    </li>;
+  }
+}
+
+export default EventSuggestion;
--- src/components/header/SearchSuggestion.js
+++ src/components/header/SearchSuggestion.js
@@ -1,33 +1,27 @@
 ﻿import React, { PureComponent } from 'react';
-import { NavLink } from 'react-router-dom';
-import EventLocation from '../event-global/EventLocation';
-import EventDate from '../event-global/EventDate';
+import EventSuggestion from './EventSuggestion';
 import { getValueFromParams, createMarkupText } from '../../helper';
 import { categories } from '../../fixtures';
 
 class SearchSuggestion extends PureComponent {
-  handleMouseDown = (event) => {
-    //ToDo The Issue should be solved by React tools
-    window.location = event.currentTarget.href;
-  }
-
   render() {
     const { eventList } = this.props;
+    const eventElement = eventList.map((event) => {
+    const title = createMarkupText(event.title.rendered);
+    const category = getValueFromParams(categories, event.categories[0], 'id', 'url');
+    const url = `/events/${event.acf.cities}/${category}/${event.id}`;
 
-    let eventElement = eventList.map((event) => {
-        const category = getValueFromParams(categories, event.categories[0], 'id', 'url');
-        const url = `/events/${event.acf.cities}/${category}/${event.id}`;
+    return <EventSuggestion
+             eventID={event.id}
+             title={title}
+             city={event.acf.cities}
+             address={event.acf.location}
+             date={event.acf.dateOf}
+             category={category}
+             url={url}
+      />
+    });
 
-        return <li key={event.id}>
-          <NavLink to={url} onMouseDown={this.handleMouseDown}>
-            <span dangerouslySetInnerHTML={createMarkupText(event.title.rendered)} />
-            <div>
-              <EventLocation city={event.acf.cities} address='' />
-              <EventDate date={event.acf.dateOf} />
-            </div>
-          </NavLink>
-        </li>;
-      });
     return (
       <ul className='header__search-suggestion'>
         {eventElement}
