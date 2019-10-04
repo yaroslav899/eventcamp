19bf397e50fa944b356adee3004dbe9d36361f6a
ASTOUND\y.volokh <y.volokh@astoundcommerce.com>
2019-10-04 17:50:24 +0300

    Fixed some issue. Optimized code

--- src/components/event-filters/SelectFilter.js
+++ src/components/event-filters/SelectFilter.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CityField from './CityField';
 import CategoryField from './CategoryField';
 import TopicField from './TopicField';
 
-class SelectFilter extends Component {
-  render() {
-    return (
-      <div className="event-filter-option">
-        <CityField changeHistory={true} />
-        <CategoryField />
-        <TopicField />
-      </div>
-    );
-  }
-}
+const SelectFilter = () => {
+  return (
+    <div className="event-filter-option">
+      <CityField changeHistory={true} />
+      <CategoryField />
+      <TopicField />
+    </div>
+  );
+};
 
 export default SelectFilter;
