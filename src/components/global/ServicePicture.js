09e92a209a6e7fe04fe61e861490c79115683817
yaroslav899 <yaroslav899@gmail.com>
2019-11-24 10:38:37 +0200

    Fixed issue with Main page and chosen city

--- src/components/global/ServicePicture.js
+++ src/components/global/ServicePicture.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { categories } from '../../fixtures';
+import { getValueFromParams } from '../../helper';
+import { cities } from '../../fixtures';
 
-const ServicePicture = () => {
-  return categories.map(category => <div key={category.id} className="col-6 col-sm-4 text-center">
-    <NavLink to={`/events/any/${category.url}`}>
-      <img src={category.img} alt={category.name} title={category.name} className="img-fluid" />
-      <br />
-      <p>{category.name}</p>
-    </NavLink>
-  </div>);
+class ServicePicture extends PureComponent {
+  render() {
+    const { city, defaultCity } = this.props;
+    const eventCity = getValueFromParams(cities, city, 'id', 'url');
+    const categoryBanners = categories.map(category => <div key={category.id} className="col-6 col-sm-4 text-center">
+      <NavLink to={`/events/${eventCity || defaultCity}/${category.url}`}>
+        <img src={category.img} alt={category.name} title={category.name} className="img-fluid" />
+        <br />
+        <p>{category.name}</p>
+      </NavLink>
+    </div>);
+    return (
+      <Fragment>
+        {categoryBanners}
+      </Fragment>
+    )
+  }
+}
+
+const mapStateToProps = storeData => {
+  return { city: storeData.filterState.cities };
 };
 
-export default ServicePicture;
+ServicePicture.defaultProps = {
+  defaultCity: 'any',
+};
+
+export default connect(mapStateToProps)(ServicePicture);
